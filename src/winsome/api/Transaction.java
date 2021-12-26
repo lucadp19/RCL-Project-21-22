6c71e9637628ee43cf3bf460931072d254a9225b
lucadp19 <luca.dp1909@gmail.com>
2021-12-26 16:45:48 +0100

    add Transaction class

---
+++ src/winsome/api/Transaction.java
@@ -0,0 +1,15 @@
+package winsome.api;
+
+import java.time.Instant;
+
+public class Transaction {
+    public final String user;
+    public final int increment;
+    public final Instant timestamp;
+    
+    public Transaction(String user, int increment){
+        this.user = user;
+        this.increment = increment;
+        this.timestamp = Instant.now();
+    }
+}
