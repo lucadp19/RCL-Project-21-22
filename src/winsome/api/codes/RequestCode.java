61ab0e9e526d307548231aa68f8fa1f7efa00de0
luca <luca.dp1909@gmail.com>
2022-01-02 11:18:57 +0100

    add functions to get/set request/response fields in json objects

--- src/winsome/api/codes/RequestCode.java
+++ src/winsome/api/codes/RequestCode.java
@@ -1,19 +1,79 @@
 package winsome.api.codes;
 
+import com.google.gson.JsonObject;
+
+import winsome.api.exceptions.MalformedJSONException;
+
+/** A Request code in the communication Client-Server */
 public enum RequestCode {
+    /** Login request */
     LOGIN,
+    /** Logout request */
     LOGOUT,
+    /** Get Winsome Users */
     SHOW_USERS,
+    /** Follow request */
     FOLLOW,
+    /** Unfollow request */
     UNFOLLOW,
+    /** List current user's posts */
     BLOG,
+    /** New post request */
     POST,
+    /** Get feed request */
     FEED,
+    /** Show a post */
     SHOW_POST,
+    /** Delete a post */
     DELETE_POST,
+    /** Rewin a post */
     REWIN_POST,
+    /** Rate a post */
     RATE_POST,
+    /** Add a comment to a post */
     COMMENT,
+    /** Get wallet */
     WALLET,
-    WALLET_BTC
+    /** Get wallet in bitcoins */
+    WALLET_BTC;
+
+    /** Name of the request field in Json representation */
+    private static final String requestFieldName = "request-code";
+
+    /**
+     * Adds a request code to a Json object.
+     * @param json the given json
+     * @param code the request code
+     * @throws NullPointerException if either json or code are null
+     */
+    public static void addRequestToJson(JsonObject json, RequestCode code){
+        if(json == null || code == null) throw new NullPointerException();
+
+        json.addProperty(requestFieldName, code.toString());
+    }
+
+    /**
+     * Adds this request code to a Json object.
+     * @param json the given json
+     * @throws NullPointerException if either json or code are null
+     */
+    public void addRequestToJson(JsonObject json){
+        RequestCode.addRequestToJson(json, this);
+    }
+
+    /**
+     * Gets a request code from a Json message.
+     * @param json the given json
+     * @return the request code contained in the given json
+     * @throws MalformedJSONException if the given Json does not contain a valid request code 
+     */
+    public static RequestCode getRequestFromJson(JsonObject json) throws MalformedJSONException {
+        if(json == null) throw new NullPointerException();
+
+        try { return RequestCode.valueOf(json.get(requestFieldName).getAsString()); }
+        catch(ClassCastException | IllegalStateException | NullPointerException | IllegalArgumentException ex){ 
+            throw new MalformedJSONException("the given json did not contain a valid request code field"); 
+        }
+    }
+ 
 }
--- src/winsome/api/codes/ResponseCode.java
+++ src/winsome/api/codes/ResponseCode.java
@@ -1,10 +1,60 @@
 package winsome.api.codes;
 
+import com.google.gson.JsonObject;
+
+import winsome.api.exceptions.MalformedJSONException;
+
+/** A Response code for the communication Server-Client */
 public enum ResponseCode {
+    /** Successful operation */
     SUCCESS,
+    /** Request was malformed */
     MALFORMED_JSON_REQUEST,
+    /** The given user is not signed up */
     USER_NOT_REGISTERED,
+    /** Wrong password in login */
     WRONG_PASSW,
+    /** User or client is already logged */
     ALREADY_LOGGED,
+    /** Fatal communication error */
     FATAL_ERR;
+
+    /** Name of the response field in Json representation */
+    private static final String responseFieldName = "response-code";
+
+    /**
+     * Adds a response code to a Json object.
+     * @param json the given json
+     * @param code the response code
+     * @throws NullPointerException if either json or code are null
+     */
+    public static void addResponseToJson(JsonObject json, ResponseCode code){
+        if(json == null || code == null) throw new NullPointerException();
+
+        json.addProperty(responseFieldName, code.toString());
+    }
+
+    /**
+     * Adds this response code to a Json object.
+     * @param json the given json
+     * @throws NullPointerException if either json or code are null
+     */
+    public void addResponseToJson(JsonObject json){
+        ResponseCode.addResponseToJson(json, this);
+    }
+
+    /**
+     * Gets a response code from a Json message.
+     * @param json the given json
+     * @return the response code contained in the given json
+     * @throws MalformedJSONException if the given Json does not contain a valid response code 
+     */
+    public static ResponseCode getResponseFromJson(JsonObject json) throws MalformedJSONException {
+        if(json == null) throw new NullPointerException();
+
+        try { return ResponseCode.valueOf(json.get(responseFieldName).getAsString()); }
+        catch(ClassCastException | IllegalStateException | NullPointerException | IllegalArgumentException ex){ 
+            throw new MalformedJSONException("the given json did not contain a valid response code field", ex); 
+        }
+    }
 }
