f7dec3150daceb0bb73217ec02de23b8ba616228
luca <luca.dp1909@gmail.com>
2022-01-08 17:08:59 +0100

    fix: dirs.sh created wrong directories

--- scripts/dirs.sh
+++ scripts/dirs.sh
@@ -3,9 +3,9 @@
 BLUE='\033[1;34m'
 RESET='\033[0m'
 
-if [ ! -d bin ] || [ ! -d obj ] || [ ! -d logs ]
+if [ ! -d bin ] || [ ! -d out ] || [ ! -d logs ]
 then
     echo -e "${BLUE}-> ${RESET}Creating ./bin, ./obj and ./logs directories..."
-    mkdir -p bin/ obj/util obj/util/hash obj/api obj/server obj/client
+    mkdir -p bin/ out/ logs/
     echo -e "${BLUE}==> Directories created!${RESET}\n"
 fi
